a9006e4e733d6acdefdbca77ef56e50284428e95
baehrendtz <fabian.baehrendtz@gmail.com>
2025-12-05 14:24:32 +0100

    Add user terms, privacy policy, contact, solutions, about us, and pricing pages
    - Created 'anvandarvillkor.astro' for user terms with sections on acceptance, service description, user account, payment, user responsibility, liability, intellectual property, termination, changes, applicable law, and contact.
    - Created 'integritetspolicy.astro' for privacy policy detailing data collection, usage, legal basis, storage, rights, cookies, contact, and supervisory authority.
    - Created 'kontakt.astro' for contact page with a form for inquiries and company information.
    - Created 'losningar.astro' for solutions offered, including salary management, driver reports, and flexible configuration.
    - Created 'om-oss.astro' for about us page detailing company history, values, and contact information.
    - Created 'priser.astro' for pricing page with tiered plans, enterprise features, and FAQs.

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,7 +5,7 @@ import icon from "astro-icon";
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://payflo.dev',
+  site: 'https://taxiadministration.se',
   output: 'static',
   prefetch: true,
   integrations: [
